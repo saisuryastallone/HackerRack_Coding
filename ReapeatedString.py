799041e9dade16e4ab93439795514193fce7cd4e
Sai Surya Pavuluri <65032235+saisuryastallone@users.noreply.github.com>
2021-06-10 18:06:24 +0100

    Create ReapeatedString.py

---
+++ ReapeatedString.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    
+    r = 0
+    l = len(s)
+    for i in range(0, l):
+        if s[i] == 'a':
+            r+= 1
+    r*= int(n / l)
+    for i in range(0, n % l):
+        if s[i] == 'a':
+            r+= 1
+    return r
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
